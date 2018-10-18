9c91803c89ec1a56134a97bb35baec438df13861
Nhat Tran Trung <Tran Trung Nhat@TRUNGNHAT-KT.tdif.net>
2018-10-18 15:39:38 +0700

    Lecture 197 Completed

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,1 +1,34 @@
-<h1>Admin Users page</h1>
+@extends ('layouts.admin')
+
+@section('content')
+<h1>All users listing</h1>
+
+ <table class="table">
+     <thead>
+       <tr>
+         <th>Id</th>
+         <th>Name</th>
+         <th>Email</th>
+         <th>created_at</th>
+         <th>updated_at</th>
+       </tr>
+     </thead>
+     <tbody>
+     @if($users)
+         @foreach($users as $user)
+             <tr>
+                 <td>{{$user->id}}</td>
+                 <td>{{$user->name}}</td>
+                 <td>{{$user->email}}</td>
+                 <td>{{$user->created_at->diffForHumans()}}</td>
+                 <td>{{$user->updated_at->diffForHumans()}}</td>
+             </tr>
+         @endforeach
+
+     @endif
+
+
+     </tbody>
+   </table>
+
+@stop
